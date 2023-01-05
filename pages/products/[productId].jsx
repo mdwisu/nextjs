71dd7a1cea4cb03d92a4827780670107a720ee5e
mdwisu <dwisusanto784@gmail.com>
2023-01-05 20:25:39 +0700

    best practice

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-export default function ProductDetail({ product }) {
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    throw new Error(data.message);
+  }
+  return data;
+};
+
+export default function ProductDetail() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const router = useRouter();
+  const { productId } = router.query;
+  const { data, error } = useSWR(
+    () => productId && `http://localhost:5000/products/${productId}`,
+    fetcher
+  )
+
+  if (error) return <div>{error.message}</div>
+  if (!data) return <div>Loading...</div>
+  // useEffect(() => {
+  //   const getProductById = async () => {
+  //     const response = await fetch(
+  //       `http://localhost:5000/products/${productId}`
+  //     );
+  //     console.log(response);
+  //     const data = await response.json();
+  //     console.log(data);
+  //     setName(data.name);
+  //     setPrice(data.price);
+  //   };
+  //   getProductById();
+  // }, [productId]);
   return (
     <div>
-      {product.name} - {product.price}
+      {data.name} - {data.price}
     </div>
   );
-}
-
-export async function getServerSideProps({ params }) {
-  const res = await fetch(`http://localhost:5000/products/${params.productId}`);
-  const product = await res.json();
-
-  if (!product.id) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      product,
-    },
-  };
 }
--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,8 +1,27 @@
-export default function Products({ products }) {
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      host: process.env.HOST,
+    },
+  };
+};
+
+export default function Products({ host }) {
+  console.log(host);
+  const { data, error } = useSWR(`${host}/products`, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div>
       <ul>
-        {products.map((product) => (
+        {data.map((product) => (
           <li key={product.id}>
             {product.name} - {product.price}
           </li>
@@ -10,14 +29,4 @@ export default function Products({ products }) {
       </ul>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  const res = await fetch('http://localhost:5000/products');
-  const products = await res.json();
-  return {
-    props: {
-      products,
-    },
-  };
 }
